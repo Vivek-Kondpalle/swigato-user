4efc561126d2316f81846e49f82f97cb95f38984
Vivek Kondpalle <vukondpalle@gmail.com>
2024-03-24 18:36:19 +0530

    Home Page Card View WIP

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -1,17 +1,16 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, FlatList, SafeAreaView } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
+import Card from '../UI/Card/card';
+import { restaurantDetails } from '../utils/restaurantsData';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-      <Text>HomeScreen</Text>
-      <Pressable onPress={() => navigation.navigate('RestaurantDetails')}>
-        <Text>RestaurantDetailsPage</Text>
-      </Pressable>
-    </View>
+    <SafeAreaView style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+        <FlatList data={restaurantDetails} keyExtractor={item => item.id} showsVerticalScrollIndicator={false} renderItem={(item) => <Card detail={item} />} />
+    </SafeAreaView>
   )
 }
 
