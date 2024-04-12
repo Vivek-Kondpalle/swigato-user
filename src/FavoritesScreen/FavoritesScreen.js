876e8978f29bd08734ca57d1acc8469371105e36
Vivek Kondpalle <vukondpalle@gmail.com>
2024-04-13 00:59:58 +0530

    Basic address, favorites, profile, userProfile screen created

--- src/FavoritesScreen/FavoritesScreen.js
+++ src/FavoritesScreen/FavoritesScreen.js
@@ -1,12 +1,22 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, FlatList, SafeAreaView } from 'react-native'
 import React from 'react'
+import { restaurantDetails } from '../utils/restaurantsData'
+import Card from '../UI/Card/card'
 
 const FavoritesScreen = () => {
 
   return (
-    <View>
-      <Text>FavoritesScreen</Text>
-    </View>
+    <SafeAreaView style={{height: '100%'}}>
+        <View style={{ display: 'flex', justifyContent: 'center',  width: '100%' }}>
+          <FlatList 
+            data={restaurantDetails} 
+            keyExtractor={item => item.id} 
+            showsVerticalScrollIndicator={false} 
+            renderItem={(item) => <Card detail={item} />} 
+            contentContainerStyle={{ paddingBottom: 40 }}
+          />
+        </View>
+    </SafeAreaView>
   )
 }
 
